bc1f8ae723c1066d5deb20c981276897bf71f242
YashasJani <93638633+YashasJani@users.noreply.github.com>
2022-06-21 19:03:01 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var thief
 var thiefAnimation
 var cityBG
-
+var bg
 function preload(){
   thiefAnimation=loadAnimation("assets/theif1.png","assets/theif2.png")
   cityBG=loadImage("assets/citybg.jpg")
@@ -9,12 +9,20 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
- thief=createSprite(380,600,50,30)
+ 
+ bg=createSprite(windowWidth/2,windowHeight/7,windowWidth,windowHeight)
+ bg.addImage(cityBG)
+ bg.velocityX=-3
+bg.scale=1.6
+thief=createSprite(380,600,50,30)
  thief.addAnimation("running",thiefAnimation)
  thief.scale=0.7
 }
 
 function draw() {
-  background(cityBG);
+  background(0);
+  if(bg.x<0){
+    bg.x=bg.width/5
+  }
   drawSprites()
 }
